0fa075ab8e56a265fc448eb1372c85762ac95b29
Dave Nicholson <fatnic388@gmail.com>
2015-10-09 16:43:12 +0100

    added struct for wall segments

--- include/structs.hpp
+++ include/structs.hpp
@@ -6,16 +6,30 @@ typedef sf::Vector2<float> Point;
 
 struct Ray
 {
-    double angle;
     Point start, end;
+};
+
+struct Segment
+{
+    Segment(const Point p1, const Point p2)
+        : p1(p1)
+	    , p2(p2)
+    {}
+    Point p1, p2;
 };
 
 struct Wall
 {
+	Segment* segments[4];
     sf::IntRect AABB;
 
     Wall(const int x, const int y, const int width, const int height)
     {
+	    segments[0] = new Segment(Point(x, y), Point(x + width, y));
+        segments[1] = new Segment(Point(x + width, y), Point(x + width, y + height));
+        segments[2] = new Segment(Point(x + width, y + height), Point(x, y + height));
+        segments[3] = new Segment(Point(x, y + height), Point(x, y)); 
+
         AABB.left = x;
         AABB.top = y;
         AABB.width = width;
